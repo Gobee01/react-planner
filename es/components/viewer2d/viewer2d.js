5c8b124378d6054ce1d089f7a4123b4df214966d
gobeeselva001 <gobeeselva001@gmail.com>
2023-05-22 21:28:05 +0530

    nandayo

--- es/components/viewer2d/viewer2d.js
+++ es/components/viewer2d/viewer2d.js
@@ -7,7 +7,7 @@ import State from './state';
 import * as SharedStyle from '../../shared-style';
 import { RulerX, RulerY } from './export';
 
-export function cursorPosition({ x, y }) {
+export function cursorPosition(x, y) {
   return { x: x, y: y };
 }
 
@@ -201,7 +201,7 @@ export default function Viewer2D(_ref, _ref2) {
         y = _mapCursorPosition2.y;
         console.log("xx=",_mapCursorPosition2.x)
         console.log("yy=",_mapCursorPosition2.y)
-        cursorPosition({x, y})
+        cursorPosition(_mapCursorPosition2.x, _mapCursorPosition2.y)
 
     if (mode === constants.MODE_IDLE) {
       var elementData = extractElementData(event.target);
